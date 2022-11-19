b6820fde878668366c649978596fa24a3088e0a3
Takahiro Yoshinaga <t.yoshinaga0106@gmail.com>
2022-11-19 15:27:26 +0900

    Update About.py

--- About.py
+++ About.py
@@ -6,5 +6,5 @@ st.title("AB testing tools")
 st.write("Calculate sample size / Statistical test (under constructions.)")
 
 # image
-image = Image.open('./nakamura/images/ab.jpeg')
+image = Image.open('./abtools/images/ab.jpeg')
 st.image(image, use_column_width=True)
