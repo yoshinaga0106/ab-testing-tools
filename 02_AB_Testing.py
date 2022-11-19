01f24a3deaded77b4644264adf2d4b48f8092cd9
Takahiro Yoshinaga <t.yoshinaga0106@gmail.com>
2022-11-19 15:09:44 +0900

    Update 02_AB_Testing.py

--- 02_AB_Testing.py
+++ 02_AB_Testing.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from PIL import Image
 
-st.title("超高性能人工知能なかむら君2号: AB Testing")
-st.write("人工知能のなかむら君がABテストの結果を検定してくれます")
+st.title("AB Testing")
+st.write("Under Construnctions")
 
 # image
-image = Image.open('./nakamura/images/c3po.jpeg')
-st.image(image, caption='なかむら君',use_column_width=True)
+image = Image.open('./nakamura/images/underconstuctions.jpeg')
+st.image(image, use_column_width=True)
