01a9f44818a04b5cec19fe54d19943786baf2851
Takahiro Yoshinaga <t.yoshinaga0106@gmail.com>
2022-11-28 14:56:51 +0900

    ratio

--- api/main.py
+++ api/main.py
@@ -1,8 +1,12 @@
 from enum import Enum
 from pydantic import BaseModel, Field
 
+import numpy as np
 from math import sqrt
-from statsmodels.stats.power import tt_ind_solve_power 
+from scipy.special import comb
+from statsmodels.stats.power import tt_ind_solve_power, zt_ind_solve_power
+from statsmodels.stats.proportion import proportion_effectsize
+
 from fastapi import FastAPI
 
 class Alternative(Enum):
@@ -17,10 +21,12 @@ class SampleSizeBase(BaseModel):
     lift: float = Field(..., ge = 0.0)
     alternative: Alternative
 
-
 class SampleSizeAvg(SampleSizeBase):
     avg: float = Field(..., ge = 0.0)
     var: float = Field(..., ge = 0.0)
+
+class SampleSizeRatio(SampleSizeBase):
+    rate: float = Field(..., ge = 0.0, le = 1.0)
 
 app = FastAPI()
 
@@ -28,14 +34,31 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/stats/avg")
+@app.post("/stats/avg/samplesize/")
 async def calc_sample_size(params: SampleSizeAvg):
-    # OK: requests.post("http://127.0.0.1:8000/stats/avg", json = {"avg": 1.0, "var": 1.0, "alpha": 0.05, "power": 0.8, "group": 2, "lift": 0.03, "alternative": "two-sided"})
+    # OK: requests.post("http://127.0.0.1:8000/stats/avg/samplesize", json = {"avg": 1.0, "var": 1.0, "alpha": 0.05, "power": 0.8, "group": 2, "lift": 0.03, "alternative": "two-sided"})
+    e_alpha = params.alpha / comb(params.group, 2)
     effect_size = params.lift * params.avg / sqrt(params.var)
     sample_size = tt_ind_solve_power(
         effect_size = effect_size,
-        alpha = params.alpha,
+        alpha = e_alpha,
         power = params.power,
         alternative = params.alternative.value
     )
+    sample_size = 0.0 if type(sample_size) is np.ndarray else sample_size
+    return {"params": params, "sample size": sample_size}
+
+@app.post("/stats/ratio/samplesize/")
+async def calc_sample_size(params: SampleSizeRatio):
+    # OK: requests.post("http://127.0.0.1:8000/stats/ratio/samplesize", json = {"avg": 1.0, "var": 1.0, "alpha": 0.05, "power": 0.8, "group": 2, "lift": 0.03, "alternative": "two-sided"})
+    e_alpha = params.alpha / comb(params.group, 2)
+    effect_size = proportion_effectsize((1 + params.lift) * params.rate, params.rate)
+    sample_size = zt_ind_solve_power(
+            effect_size=effect_size,
+            alpha=e_alpha,
+            power=params.power,
+            ratio=1 + params.lift,
+            alternative=params.alternative.value,
+        )
+    sample_size = 0.0 if type(sample_size) is np.ndarray else sample_size
     return {"params": params, "sample size": sample_size}
